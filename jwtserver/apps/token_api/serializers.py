a2215d529eac22e073cb59fce4ef9a8d74749af6
Virgile Gerecke <virgile.gerecke@unistra.fr>
2019-09-04 11:14:49 +0200

    🐛 Handling Spore error when fetching additional data. Closes #3

--- jwtserver/apps/token_api/serializers.py
+++ jwtserver/apps/token_api/serializers.py
@@ -1,6 +1,7 @@
 import datetime
 
 import sentry_sdk
+from britney.errors import SporeMethodStatusError
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -17,6 +18,7 @@ class UserTokenSerializer(serializers.Serializer, ):
     """
     Token serializer
     """
+
     def validate_user(self, attrs, user):
         data = super().validate(attrs)
         refresh = self.get_token(user)
@@ -31,6 +33,7 @@ class TokenObtainCASSerializer(UserTokenSerializer):
     """
     Generates token in exchange of a CAS ticket
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['ticket'] = serializers.CharField()
@@ -38,9 +41,9 @@ class TokenObtainCASSerializer(UserTokenSerializer):
 
     def get_token(self, user):
         t = RefreshToken.for_user(user)
-        #t['iss'] = self.context['request'].get_host()
+        # t['iss'] = self.context['request'].get_host()
         t['iss'] = 'Ernest'
-        #t['issuer'] = self.context['request'].get_host()
+        # t['issuer'] = self.context['request'].get_host()
         t['sub'] = user.username
         t['nbf'] = datetime.datetime.now().timestamp()
         try:
@@ -65,20 +68,23 @@ class TokenObtainCASSerializer(UserTokenSerializer):
         :return: None
         """
         if not hasattr(user, 'additionaluserinfo'):
-            response = get_user(username=user.username)
-            if len(response.data['results']) > 0:
-                c_user = response.data['results'][0]
-                if 'accounts' in c_user:
-                    for account in c_user['accounts']:
-                        if account['username'] == user.username:
-                            directory_id = account['directory_id']
-                            print(directory_id)
-                            goc = AdditionalUserInfo.objects.get_or_create(user=user)
-                            if goc[1]:
-                                goc[0].directory_id = directory_id
-                                goc[0].save()
-            else:
-                sentry_sdk.capture_message("No Camelot result for username {}".format(user.username))
+            try:
+                response = get_user(username=user.username)
+                if len(response.data['results']) > 0:
+                    c_user = response.data['results'][0]
+                    if 'accounts' in c_user:
+                        for account in c_user['accounts']:
+                            if account['username'] == user.username:
+                                directory_id = account['directory_id']
+                                print(directory_id)
+                                goc = AdditionalUserInfo.objects.get_or_create(user=user)
+                                if goc[1]:
+                                    goc[0].directory_id = directory_id
+                                    goc[0].save()
+                else:
+                    sentry_sdk.capture_message("No Camelot result for username {}".format(user.username))
+            except SporeMethodStatusError as e:
+                sentry_sdk.capture_exception(e)
 
 
 class TokenObtainDummySerializer(UserTokenSerializer):
