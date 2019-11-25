61378e052b57bdee41d764d1e6fb8af116bf68e2
Virgile Gerecke <virgile.gerecke@unistra.fr>
2019-11-25 18:35:08 +0100

    💬 Added data to token : organization and affiliations

--- jwtserver/apps/token_api/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class AdditionalUserInfo(models.Model):
-    user = models.OneToOneField(User)
-    directory_id = models.CharField(max_length=30)
--- jwtserver/libs/api/client.py
+++ jwtserver/libs/api/client.py
@@ -1,76 +1,40 @@
 import logging
-from functools import wraps
 
-import britney_utils
-from britney.errors import SporeMethodCallError, SporeMethodStatusError
-from britney.middleware.format import Json
+import ldap
+import sentry_sdk
 from django.conf import settings
 
-from .exceptions import WSError
+from jwtserver.libs.decorators import MemoizeWithTimeout
 
-_clients = {}
-
-
-def get_client(name):
-    global _clients
-
-    name = name.upper()
-    if name not in _clients:
-        client = britney_utils.get_client(
-            name,
-            getattr(settings, '%sWS_DESCRIPTION' % name),
-            base_url=getattr(settings, '%sWS_BASE_URL' % name),
-            middlewares=(
-                (Json,),
-                ('ApiKey', {
-                    'key_name': 'Authorization',
-                    'key_value': 'Token %s' % getattr(
-                        settings, '%sWS_TOKEN' % name)
-                }),
-            )
-        )
-        client.add_default('format', 'json')
-        _clients[name] = client
-
-    return _clients[name]
+logger = logging.getLogger(__name__)
 
 
-def check_status(logger_name=__name__):
-    def wrapper(func):
-        object_type = func.__name__.replace('get_', '').rstrip('s')
-        logger = logging.getLogger(logger_name)
-
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            logger.debug('Getting %s', object_type)
-            try:
-                response = func(*args, **kwargs)
-            except SporeMethodStatusError as http_error:
-                status = http_error.response.status_code
-                if status == 401:
-                    message = 'Webservice account can\'t authenticate'
-                elif status == 403:
-                    message = 'Webservice account needs some authorization'
-                elif status >= 500:
-                    message = 'Webservice seems to be down'
-                else:
-                    message = 'Error %s' % http_error.response.reason
-                logger.critical('%s: %s', status, message)
-                raise WSError(http_error.response, message, object_type)
-            except SporeMethodCallError as method_call_error:
-                message = 'Bad function call: %s' % method_call_error.cause
-                logger.critical(message)
-                logger.critical('Expected values: %s',
-                                ', '.join(method_call_error.expected_values))
-                raise WSError(None, message, object_type)
-            else:
-                logger.debug(response.request.url)
-                return response
-
-        return wrapped
-
-    return wrapper
+def get_client():
+    connexion = ldap.initialize(settings.LDAP_CONNEXION, bytes_mode=False)
+    connexion.simple_bind_s(settings.LDAP_USER, settings.LDAP_PASSWORD)
+    return connexion
 
 
+@MemoizeWithTimeout(timeout=86400)
 def get_user(username):
-    return get_client('camelot').get_persons(username=username)
+    results = get_client().search_s(settings.LDAP_BRANCH, ldap.SCOPE_SUBTREE, settings.LDAP_FILTER.format(username))
+    if len(results) != 1:
+        logger.error(f'Received {len(results)} results for query on {username}')
+        sentry_sdk.capture_message(f'Received {len(results)} results for query on {username}')
+        return None
+    people = results[0]
+
+    def get_attr(attr):
+        return people[1][attr][0].decode("utf-8")
+
+    def get_attrs(attr):
+        return [x.decode("utf-8") for x in people[1][attr]]
+
+    def get_ordered_attrs(primary_attr, attr):
+        return get_attrs(primary_attr) \
+               + list(set(get_attrs(attr)) - set(get_attrs(primary_attr)))
+
+    return {'username': get_attr('uid'),
+            'organization': get_attr('supannEtablissement'),
+            'affiliations': get_ordered_attrs('eduPersonPrimaryAffiliation', 'eduPersonAffiliation'),
+            'directory_id': get_attr('udsDirectoryId')}
