e9811bf3eb6e9b7302483ae5fa8b6e68ae8b169d
Virgile Gerecke <virgile.gerecke@unistra.fr>
2020-10-14 09:00:24 +0200

    💄 Admin - Adds PrettyJSONFields in authorized services

--- jwtserver/apps/token_api/admin.py
+++ jwtserver/apps/token_api/admin.py
@@ -1,10 +1,34 @@
+import json
+
 from django.contrib import admin
+from django.contrib.postgres.forms.jsonb import InvalidJSONInput, JSONField as JSONFormField
+from django import forms
 
 from .models import (AuthorizedService, )
+
+class PrettyJSONField(JSONFormField):
+    # https://code.djangoproject.com/ticket/29150
+    # https://code.djangoproject.com/ticket/26482
+
+    def prepare_value(self, value):
+        if isinstance(value, InvalidJSONInput):
+            return value
+        return json.dumps(value, indent=4)
+
+
+class AuthorizedServiceForm(forms.ModelForm):
+    class Meta:
+        field_classes = {
+            'data': PrettyJSONField
+        }
+        widgets = {
+            'data': forms.Textarea(attrs={'rows': 20, 'cols': 80})
+        }
 
 
 @admin.register(AuthorizedService)
 class AuthorizedServiceAdmin(admin.ModelAdmin):
+    form = AuthorizedServiceForm
     list_display = ('__str__', 'keys')
 
     def keys(self, obj):
